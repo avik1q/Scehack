9c6f5305d15d38fa367f825413b914781fa6c6aa
avi karbol <avik1q@gmail.com>
2017-05-15 18:18:41 +0300

    test press btn

--- MainActivity.java
+++ MainActivity.java
@@ -4,14 +4,53 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import net.atomation.atomationsdk.api.IMultiSenseAtom;
+import net.atomation.atomationsdk.api.IOnButtonPressedListener;
+import net.atomation.atomationsdk.api.ISensorsReadListener;
+import net.atomation.atomationsdk.api.SensorsData;
+import net.atomation.atomationsdk.ble.MultiSenseAtomManager;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "SCE";
-
+    public static final String MAC="48:1A:00:14:E1";
+    public IMultiSenseAtom Atom;
     @Override
     protected  void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate: ok go!!");
+        Atom= MultiSenseAtomManager.getInstance(this.getApplicationContext())
+                .getMultiSenseAtom(MAC);
+        if(Atom==null)
+        {
+            Atom= MultiSenseAtomManager.getInstance(this.getApplicationContext())
+                    .createMultiSenseDevice(MAC);
+        }
+
+        Atom.activate();
+        Atom.setSensorsReadListener(new ISensorsReadListener() {
+            @Override
+            public void onRead(SensorsData sensorsData) {
+                Log.d(TAG, "onRead() called with: sensorsData = [" + sensorsData + "]");
+            }
+
+            @Override
+            public void onError(int errorCode) {
+                Log.d(TAG, "onError() called with: errorCode = [" + errorCode + "]");
+            }
+        });
+        Atom.setButtonPressedListener(new IOnButtonPressedListener() {
+            @Override
+            public void onPress(int reason) {
+                Log.d(TAG, "onPress() called with: reason = [" + reason + "]");
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Atom.deactivate();
     }
 }
